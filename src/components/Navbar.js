164eff1e61988a4894f0f81b8af31aaaa00cfbbd
Dustin Tran <dustin.andy.tran@gmail.com>
2021-05-05 20:26:55 +0900

    added link to GDrive resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ function Navbar(props){
     return (
     <div>
         Navbar Placeholder
+        <a href='https://drive.google.com/file/d/1LosdNYIvZtSxgA6LlyL0nMbb1uG4piI4/view?usp=sharing' target='_blank'>Resume/CV</a>
     </div>
     )
 }
