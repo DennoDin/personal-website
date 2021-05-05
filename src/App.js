087723135b6214287f5700c9b33373bd01aed5bd
Dustin Tran <dustin.andy.tran@gmail.com>
2021-05-05 20:31:45 +0900

    added Title component to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Title from './components/Title'
 import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Title></Title>
       <Navbar></Navbar>
     </div>
   );
